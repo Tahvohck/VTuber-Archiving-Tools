7933cf1c938152df767f5454a7355c1ede72a9ff
Tahvohck <Tahvohck@gmail.com>
2021-11-06 08:04:38 -0400

    First pass implementation of alt-accounts sensor script

---
+++ shame-duplicate-checker.ps1
@@ -0,0 +1,51 @@
+$check = @{}; $donos | %{
+	if ($aggr[$_.donator].donations -gt 1) {
+		$check[$_.timestamp.Date.ToString("yy\-MM\-dd")] = @{}
+	}
+};
+$donos | %{
+	if ($aggr[$_.donator].donations -gt 1) {
+		$tstamp = $_.timestamp.Date.ToString("yy\-MM\-dd")
+		if ($null -eq $check[$tstamp][$_.donator]) {
+			$check[$tstamp][$_.donator] = [pscustomobject]@{
+				Name = $_.donatorName
+				ID = $_.donator
+				date = $tstamp
+				amount = 0
+			}
+		}
+		$check[$tstamp][$_.donator].amount += $_.USD_Equivalent
+	}
+}
+$dailyTotals = foreach($date in $check.GetEnumerator()) {
+	$date.Value.GetEnumerator() | %{
+		$_.Value.amount = [math]::Round($_.Value.amount)
+		[pscustomobject]$_.Value
+	}
+}
+$dailyTotals = $dailyTotals | sort date
+$candidates = $dailyTotals | ?{ $_.amount -gt 450 } | sort Name
+$ExcludeBits = @(
+	"KFP","the","deadbeat","ch.","Lord"
+)
+foreach($candidate in $candidates) {
+	$nameBits = @($candidate.Name.Split(" ") | 
+		?{$_ -notin $ExcludeBits } |
+		?{$_.Length -gt 2 } | 
+		%{ $esc = $_ -replace "\W",""; "*${esc}*"})
+	$dailyMatches = $dailyTotals | ?{
+		$hasNameBit = $false
+		foreach ($bit in $nameBits) {
+			$hasNameBit = $hasNameBit -or ($_.name -like $bit)
+		}
+		$dateMatch = $_.date -eq $candidate.date
+		$IDsDiffer = $_.ID -ne $candidate.ID
+		$dateMatch -and $hasNameBit -and $IDsDiffer
+	}
+	if ($dailyMatches.count -gt 0) {
+		Write-Host -fore Cyan ("--- Duplicate candidates for {0} {1} [{2}] ---" -f $candidate.date,$candidate.Name,$candidate.amount)
+		$dailyMatches | ft -AutoSize ID,@{E={$_.amount};N="USD_Equ"},Name
+	}
+}
+$candidates = $candidates | sort -Unique Name
+#$candidates | ft Name,ID
